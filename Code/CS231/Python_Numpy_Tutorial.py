9be98d3aa16b654a61787448b6d905e7bdca78d9
rezaAdinepour <r3zaadinep0ur@gmail.com>
2024-02-24 22:55:47 +0330

    add boolean types

--- Code/CS231/Python_Numpy_Tutorial.py
+++ Code/CS231/Python_Numpy_Tutorial.py
@@ -3,7 +3,23 @@ import numpy as np
 
 
 
-
+# variable type
 x = 3
 print(type(x))
 print(x)
+print(x + 2, x - 2, x * 2, x ** 2)
+print(-x)
+print(complex(x))
+print('-'*20)
+
+
+# boolean type
+t = True
+f = False
+print(type(t))
+print(t and f)
+print(t and not(f))
+print(t or f)
+print(t != f) # logical xor
+print('-'*20)
+
