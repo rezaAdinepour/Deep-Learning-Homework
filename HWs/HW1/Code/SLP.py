92cf7a94464b1600cfcc2ffacdc87e48b97d4fdf
rezaAdinepour <r3zaadinep0ur@gmail.com>
2024-03-12 20:31:45 +0330

    add single layer perceptron class

--- HWs/HW1/Code/SLP.py
+++ HWs/HW1/Code/SLP.py
@@ -2,6 +2,7 @@ import torch
 import torch.nn as nn
 import torch.optim as optim
 import numpy as np
+import matplotlib.pyplot as plt
 
 
 class Perceptron(nn.Module):
@@ -11,3 +12,60 @@ class Perceptron(nn.Module):
         
     def forward(self, x):
         return torch.sigmoid(self.fc(x))
+
+
+
+
+
+
+class single_layer_perceptron():
+    def __init__(self, input_neurons=2, epoch=100, learning_rate=0.01):
+        self.w = np.random.rand(input_neurons + 1) - 0.5
+        self.learning_rate = learning_rate
+
+    def predict(self, inputs):
+        summation = np.dot(inputs, self.w[1:]) + self.w[0]
+        activation = 1.0 if (summation > 0.0) else 0.0
+
+        return activation
+    
+    
+    def train(self, X, y, epochs=100):
+        for EPOCH in range(epochs):
+            fail_count = 0
+            i = 0
+            train_loss = 0
+
+            for inputs, label in zip(X, y):
+                i = i + 1
+                prediction = self.predict(inputs)
+
+                if (label != prediction):
+                    self.w[1:] += self.learning_rate * (label - prediction) * inputs.reshape(inputs.shape[0])
+                    self.w[0] += self.learning_rate * (label - prediction)
+                    fail_count += 1
+
+                # Calculate loss
+                loss = (label - prediction) ** 2
+                train_loss += loss
+
+                plt.cla()
+                plt.scatter(X[:, 0], X[:, 1], c=y, cmap='jet', marker='.')
+                line_x = np.arange(-10, 10, 0.1)
+                line_y = (-self.w[0] - self.w[1] * line_x) / self.w[2]
+                plt.plot(line_x, line_y)
+                plt.xlim(-10, 10)
+                plt.ylim(-10, 10)
+                plt.text(-10, 11, 'epoch|iter = {:2d}|{:2d}'.format(EPOCH, i), fontdict={'size': 14, 'color':  'black'})
+                plt.pause(0.01)
+
+            # Calculate accuracy
+            accuracy = (i - fail_count) / i
+
+            print(f"Epoch: {EPOCH+1}, Loss: {np.mean(train_loss):.4f}, Accuracy: {np.mean(accuracy):.4f}")
+
+                
+
+            if (fail_count == 0):
+                plt.show()
+                break
--- HWs/HW1/Code/from_scratch.py
+++ HWs/HW1/Code/from_scratch.py
@@ -40,29 +40,42 @@ learning_rate = 0.01
 w = np.random.random(num_of_inputs + 1) - 0.5
 
 
-for epoch in range(epochs):
-    fail_count = 0
-    i = 0
 
-    for inputs, label in zip(X_train.cpu().numpy(), y_train.cpu().numpy()):
-        i = i + 1
-        prediction = predict(inputs, w)
 
-        if (label != prediction):
-            w[1:] += learning_rate * (label - prediction) * inputs.reshape(inputs.shape[0])
-            w[0] += learning_rate * (label - prediction)
-            fail_count += 1
 
-            plt.cla()
-            plt.scatter(X_train[:, 0].cpu().numpy(), X_train[:, 1].cpu().numpy(), c=y_train.cpu().numpy(), cmap='jet', marker='.')
-            line_x = np.arange(-10, 10, 0.1)
-            line_y = (-w[0] - w[1] * line_x) / w[2]
-            plt.plot(line_x, line_y)
-            plt.xlim(-10, 10)
-            plt.ylim(-10, 10)
-            plt.text(-10, 11, 'epoch|iter = {:2d}|{:2d}'.format(epoch, i), fontdict={'size': 14, 'color':  'black'})
-            plt.pause(0.01)
 
-    if (fail_count == 0):
-        plt.show()
-        break
+perceptron = single_layer_perceptron(2, 1, 0.01)
+perceptron.train(X_tensor.cpu().numpy(), y_tensor.cpu().numpy(), 1)
+
+
+
+
+
+
+
+# for epoch in range(epochs):
+#     fail_count = 0
+#     i = 0
+
+#     for inputs, label in zip(X_train.cpu().numpy(), y_train.cpu().numpy()):
+#         i = i + 1
+#         prediction = predict(inputs, w)
+
+#         if (label != prediction):
+#             w[1:] += learning_rate * (label - prediction) * inputs.reshape(inputs.shape[0])
+#             w[0] += learning_rate * (label - prediction)
+#             fail_count += 1
+
+#             plt.cla()
+#             plt.scatter(X_train[:, 0].cpu().numpy(), X_train[:, 1].cpu().numpy(), c=y_train.cpu().numpy(), cmap='jet', marker='.')
+#             line_x = np.arange(-10, 10, 0.1)
+#             line_y = (-w[0] - w[1] * line_x) / w[2]
+#             plt.plot(line_x, line_y)
+#             plt.xlim(-10, 10)
+#             plt.ylim(-10, 10)
+#             plt.text(-10, 11, 'epoch|iter = {:2d}|{:2d}'.format(epoch, i), fontdict={'size': 14, 'color':  'black'})
+#             plt.pause(0.01)
+
+#     if (fail_count == 0):
+#         plt.show()
+#         break
