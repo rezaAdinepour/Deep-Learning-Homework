0ab3c2d5aea49be7d0cd1ad98e792dc9216f4775
rezaAdinepour <r3zaadinep0ur@gmail.com>
2024-03-16 16:53:15 +0330

    create notebook torch tutorial

--- Code/Utils/1-Torch_tutorial.py
+++ Code/Utils/1-Torch_tutorial.py
@@ -8,7 +8,10 @@ device = torch.device("cuda") if torch.cuda.is_available() else torch.device("cp
 
 a = torch.tensor([1, 2, 3], dtype=torch.float64, device=device)
 b = torch.tensor([3, 4, 5], dtype=torch.float64, device=device)
+
+
 c1 = torch.add(a, b)
 c2 = a + b
 c3 = a.add_(b)
+
 
