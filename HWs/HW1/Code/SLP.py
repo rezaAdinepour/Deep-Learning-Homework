e521aca896f68b3b209477a15c1fb412004610a9
rezaAdinepour <r3zaadinep0ur@gmail.com>
2024-03-21 10:12:12 +0330

    remove non-useful code

--- HWs/HW1/Code/SLP.py
+++ HWs/HW1/Code/SLP.py
@@ -1,6 +1,5 @@
 import torch
 import torch.nn as nn
-import torch.optim as optim
 import numpy as np
 import matplotlib.pyplot as plt
 from sklearn.metrics import confusion_matrix, f1_score
@@ -8,7 +7,6 @@ import seaborn as sns
 import torch.nn.functional as F
 
 
-# Define the single layer perceptron network
 class single_layer_perceptron(nn.Module):
     def __init__(self, input_size):
         super(single_layer_perceptron, self).__init__()
@@ -27,77 +25,6 @@ class MLP(nn.Module):
     def forward(self, x):
         x = F.relu(self.fc1(x))
         return torch.sigmoid(self.fc2(x))
-
-
-
-
-
-# class single_layer_perceptron():
-#     def __init__(self, input_neurons=2, epoch=100, learning_rate=0.01):
-#         self.w = np.random.rand(input_neurons + 1) - 0.5
-#         self.learning_rate = learning_rate
-
-#     def predict(self, inputs):
-#         summation = np.dot(inputs, self.w[1:]) + self.w[0]
-#         activation = 1.0 if (summation > 0.0) else 0.0
-
-#         return activation
-    
-    
-#     def train(self, X, y, epochs=100):
-#         for EPOCH in range(epochs):
-#             fail_count = 0
-#             i = 0
-#             train_loss = 0
-#             loss_history = []
-#             accuracy_history = []
-
-#             for inputs, label in zip(X, y):
-#                 i = i + 1
-#                 prediction = self.predict(inputs)
-
-#                 if (label != prediction):
-#                     self.w[1:] += self.learning_rate * (label - prediction) * inputs.reshape(inputs.shape[0])
-#                     self.w[0] += self.learning_rate * (label - prediction)
-#                     fail_count += 1
-
-#                 # Calculate loss
-#                 loss = (label - prediction) ** 2
-#                 train_loss += loss
-
-#                 loss_history.append(np.mean(train_loss))
-                
-
-#                 plt.cla()
-#                 plt.scatter(X[:, 0], X[:, 1], c=y, cmap='jet', marker='.')
-#                 line_x = np.arange(-1, 1, 0.1)
-#                 line_y = (-self.w[0] - self.w[1] * line_x) / self.w[2]
-#                 plt.plot(line_x, line_y)
-#                 plt.xlim(-0.1, 1.1)
-#                 plt.ylim(-0.1, 1.1)
-#                 plt.text(-0.1, 1.11, 'epoch|iter = {:2d}|{:2d}'.format(EPOCH, i), fontdict={'size': 14, 'color':  'black'})
-#                 plt.pause(0.01)
-
-#             # Calculate accuracy
-#             accuracy = (i - fail_count) / i
-#             accuracy_history.append(np.mean(accuracy))
-
-#             print(f"Epoch: {EPOCH+1}, Loss: {np.mean(train_loss):.4f}, Accuracy: {np.mean(accuracy):.4f}")
-
-#             if (fail_count == 0):
-#                 plt.show()
-#                 break
-#         loss_history = np.array(loss_history)
-#         accuracy_history = np.array(accuracy_history)
-
-#         print(loss_history)
-#         print(accuracy_history)
-
-#         return loss_history, accuracy_history
-    
-
-
-
 
 
 
@@ -237,27 +164,3 @@ class Single_Layer_Perceptron():
         f1 = f1_score(y, predicted_labels)
 
         return np.mean(avg_loss), np.mean(accuracy), f1
-
-
-
-
-
-
-
-
-
-    # def plot_loss(self, loss_history, epochs=100):
-    #     x = np.arange(epochs)
-    #     fig = plt.figure(figsize=(9, 7))
-    #     plt.plot(x, self.loss_history, marker='o')
-    #     plt.title("Loss history")
-    #     plt.xlabel("Epochs")
-    #     plt.ylabel("Loss")
-
-    # def plot_accuracy(self, accuracy_history, epochs=100):
-    #     x = np.arange(epochs)
-    #     fig = plt.figure(figsize=(9, 7))
-    #     plt.plot(x, self.accuracy_history, marker='o')
-    #     plt.title("Accuracy history")
-    #     plt.xlabel("Epochs")
-    #     plt.ylabel("Accuracy")
