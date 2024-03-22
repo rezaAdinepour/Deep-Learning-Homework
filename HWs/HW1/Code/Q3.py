a340afe6e6dec8cb73a9537785625e586a67e7db
rezaAdinepour <r3zaadinep0ur@gmail.com>
2024-03-22 11:27:55 +0330

    plot f(x)

--- HWs/HW1/Code/Q3.py
+++ HWs/HW1/Code/Q3.py
@@ -3,9 +3,8 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 
-
+# check GPU a
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-
 print(device)
 
 
@@ -17,7 +16,12 @@ f = torch.sin(x) + 3 * torch.pow(x, 17) - 5 * torch.pow(x, 2)
 
 
 plt.figure(figsize=(9, 7))
-plt.plot(x, f)
+
+plt.plot(x.cpu().numpy(), f.cpu().numpy(), linestyle='-', color='blue', linewidth=2)
+plt.xlabel('x', fontsize=14)
+plt.ylabel('f(x)', fontsize=14)
+plt.title('Plot of the function f(x)', fontsize=16)
+plt.grid(linestyle='--', linewidth=0.6)
 
 
 plt.show()
