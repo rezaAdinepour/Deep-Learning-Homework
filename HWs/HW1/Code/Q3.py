71e626139377ff149124fbb0cd4de8d0b1b3de7b
rezaAdinepour <r3zaadinep0ur@gmail.com>
2024-03-22 11:42:12 +0330

    data visualization compelete

--- HWs/HW1/Code/Q3.py
+++ HWs/HW1/Code/Q3.py
@@ -1,22 +1,26 @@
 import torch
 import numpy as np
 import matplotlib.pyplot as plt
+from SLP import single_layer_perceptron
 
-
-# check GPU a
+# check GPU availability
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 print(device)
 
 
-
+# create x point
 x = torch.linspace(-10, 10, 1000, device=device)
 print(x.size())
 
+# calculate f(x)
 f = torch.sin(x) + 3 * torch.pow(x, 17) - 5 * torch.pow(x, 2)
 
+# concatinare x and f
+data = torch.concatenate((x.reshape(-1, 1), f.reshape(-1, 1)), dim=1)
+print(data.size())
 
+# plot f(x)
 plt.figure(figsize=(9, 7))
-
 plt.plot(x.cpu().numpy(), f.cpu().numpy(), linestyle='-', color='blue', linewidth=2)
 plt.xlabel('x', fontsize=14)
 plt.ylabel('f(x)', fontsize=14)
