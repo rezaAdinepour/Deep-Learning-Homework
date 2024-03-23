847cfb0bc0cf28efd7f6ad20aa825dd6523903c2
rezaAdinepour <r3zaadinep0ur@gmail.com>
2024-03-23 15:02:15 +0330

    edit plots

--- HWs/HW1/Code/Q3.py
+++ HWs/HW1/Code/Q3.py
@@ -6,6 +6,25 @@ import math
 
 
 
+
+def taylor_approximation(x, n):
+    # Taylor series for sin(x) around 0
+    sin_x = sum(((-1)**i * x**(2*i+1)) / torch.math.factorial(2*i+1) for i in range(n))
+
+    # Taylor series for x^17 around 0
+    x_17 = x**17  # Since we're expanding around 0, this is just x^17
+
+    # Taylor series for x^2 around 0
+    x_2 = x**2  # Since we're expanding around 0, this is just x^2
+
+    # Combine the series
+    f_taylor = sin_x + 3*x_17 - 5*x_2
+
+    return f_taylor
+
+
+
+
 # check GPU availability
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 print(device)
@@ -17,7 +36,7 @@ print(x.size())
 
 # calculate f(x)
 f = torch.sin(x) + 3 * torch.pow(x, 17) - 5 * torch.pow(x, 2)
-f_sin = torch.sin(x)
+f_approx = taylor_approximation(x, 1)
 f_approx_sin = x - ((1/6) * torch.pow(x, 3)) + ((1/120) * torch.pow(x, 5)) - ((1/5040) * torch.pow(x, 7)) + ((1/362880) * torch.pow(x, 9))
 
 # concatinare x and f
@@ -26,9 +45,8 @@ print(data.size())
 
 
 # plot f(x)
-
 plt.figure(figsize=(9, 7))
-plt.plot(x.cpu().numpy(), f_sin.cpu().numpy(), linestyle='-', color='blue', linewidth=2, label="orginal")
+plt.plot(x.cpu().numpy(), f.cpu().numpy(), linestyle='-', color='blue', linewidth=2, label="orginal")
 plt.xlabel('x', fontsize=14)
 plt.ylabel('f(x)', fontsize=14)
 # plt.title('Plot of the function f(x)', fontsize=16)
@@ -40,20 +58,6 @@ plt.ylabel('f(x)', fontsize=14)
 # plt.title('Plot of the function f(x)', fontsize=16)
 plt.grid(linestyle='--', linewidth=0.6)
 plt.legend()
-
-# plt.figure(figsize=(9, 7))
-# plt.plot(x.cpu().numpy(), f.cpu().numpy(), linestyle='-', color='blue', linewidth=2, label="orginal")
-# plt.xlabel('x', fontsize=14)
-# plt.ylabel('f(x)', fontsize=14)
-# # plt.title('Plot of the function f(x)', fontsize=16)
-# plt.grid(linestyle='--', linewidth=0.6)
-
-# plt.plot(x.cpu().numpy(), f_approx.cpu().numpy(), linestyle='-', color='red', linewidth=2, label="approximate")
-# plt.xlabel('x', fontsize=14)
-# plt.ylabel('f(x)', fontsize=14)
-# # plt.title('Plot of the function f(x)', fontsize=16)
-# plt.grid(linestyle='--', linewidth=0.6)
-# plt.legend()
 
 
 
