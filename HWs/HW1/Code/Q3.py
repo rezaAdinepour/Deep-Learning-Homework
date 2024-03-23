7a5bd6c2bd8c53c7521d0a12c264ca32bad77050
rezaAdinepour <r3zaadinep0ur@gmail.com>
2024-03-23 11:43:56 +0330

    write taylor apptoximate for e^x

--- HWs/HW1/Code/Q3.py
+++ HWs/HW1/Code/Q3.py
@@ -20,11 +20,29 @@ data = torch.concatenate((x.reshape(-1, 1), f.reshape(-1, 1)), dim=1)
 print(data.size())
 
 # plot f(x)
+# plt.figure(figsize=(9, 7))
+# plt.plot(x.cpu().numpy(), f.cpu().numpy(), linestyle='-', color='blue', linewidth=2)
+# plt.xlabel('x', fontsize=14)
+# plt.ylabel('f(x)', fontsize=14)
+# plt.title('Plot of the function f(x)', fontsize=16)
+# plt.grid(linestyle='--', linewidth=0.6)
+
+
+f1 = torch.exp(x)
 plt.figure(figsize=(9, 7))
-plt.plot(x.cpu().numpy(), f.cpu().numpy(), linestyle='-', color='blue', linewidth=2)
+plt.plot(x.cpu().numpy(), f1.cpu().numpy(), linestyle='-', color='blue', linewidth=2, label='$e^x$')
 plt.xlabel('x', fontsize=14)
 plt.ylabel('f(x)', fontsize=14)
-plt.title('Plot of the function f(x)', fontsize=16)
+plt.title('Plot of the $e^x$', fontsize=16)
+plt.grid(linestyle='--', linewidth=0.6)
+
+
+
+f_approx = 1 + x + (0.5 * torch.pow(x, 2)) + ((1/6) * torch.pow(x, 3)) + ((1/24) * torch.pow(x, 4))
+plt.plot(x.cpu().numpy(), f_approx.cpu().numpy(), linestyle='-', color='red', linewidth=2, label="approximate")
+plt.xlabel('x', fontsize=14)
+plt.ylabel('f(x)', fontsize=14)
+plt.title('Plot of the $e^x$', fontsize=16)
 plt.grid(linestyle='--', linewidth=0.6)
 
 
