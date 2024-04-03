ebce94b32260823990261f42e95689127c33f496
rezaAdinepour <r3zaadinep0ur@gmail.com>
2024-04-03 16:46:01 +0330

    compelete Q1 and Q1 notebook

--- HWs/HW2/Code/utils.py
+++ HWs/HW2/Code/utils.py
@@ -3,43 +3,6 @@ import torch.nn.functional as F
 import torch
 
 
-
-
-# class MLP(nn.Module):
-#     def __init__(self):
-#         super(MLP, self).__init__()
-#         self.hidden = nn.Linear(2, 3)
-#         self.output = nn.Linear(3, 2)
-
-#     def forward(self, x):
-#         x = F.relu(self.hidden(x))
-#         x = self.output(x)
-#         return F.softmax(x, dim=1)
-
-
-
-
-
-# class MLP(nn.Module):
-#     def __init__(self, input_size, hidden_size, output_size):
-#         super(MLP, self).__init__()
-#         self.layer1 = nn.Linear(input_size, hidden_size, bias=True, device="cuda:0")
-#         #self.hidden2 = nn.Linear(10, 10)
-#         self.layer2 = nn.Linear(hidden_size, output_size, bias=True, device="cuda:0")
-
-#     def forward(self, x):
-#         #x = F.sigmoid(self.hidden1(x))
-#         #x = F.sigmoid(self.hidden2(x))
-#         # x = self.output(x)
-#         y_hidden = self.layer1(x)
-#         y = self.layer2(F.relu(y_hidden))
-#         return y
-#         #return F.softmax(y, dim=1)
-    
-
-
-
-
 class MLP(nn.Module):
     def __init__(self, input_size, hidden_size, output_size=2, device='cpu'):
         super(MLP, self).__init__()
